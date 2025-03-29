bfa50e0f24c1aead99933fc55100a2dfd4b6fb85
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-03-29 13:33:55 +0100

    wip

--- src/LibImageQuant.jl
+++ src/LibImageQuant.jl
@@ -24,7 +24,7 @@ end
 const ColorOrder = Symbol
 const COLOR_ORDERS = [:ARGB, :ABGR, :RGBA, :BGRA]
 
-function jl_to_c(image, order::ColorOrder)
+function jl_to_c(matrix, order::ColorOrder)
     @show order
     # order = :RGBA
     # order = Symbol(order)
@@ -32,25 +32,17 @@ function jl_to_c(image, order::ColorOrder)
     @show T
     @show T === ColorTypes.RGBA
     # T =ColorTypes.RGBA
-    image = permutedims(image)
-    image1 = ColorTypes.RGBA.(image)
-    image2 = getproperty(ColorTypes, order).(image)
-    @assert image1 == image2
-    image = image2
-    input_data = collect(reinterpret(reshape, UInt8, image))
+    matrix = permutedims(matrix)
+    matrix1 = ColorTypes.RGBA.(matrix)
+    matrix2 = getproperty(ColorTypes, order).(matrix)
+    @assert matrix1 == matrix2
+    matrix = matrix2
+    input_data = collect(reinterpret(reshape, UInt8, matrix))
     return copy(input_data)
 end
 
 function to_argb32(c::LiqColor, out_order::ColorOrder)
-    if out_order == :ARGB
-        return ARGB32(to_N0f8(c.a), to_N0f8(c.r), to_N0f8(c.g), to_N0f8(c.b))
-    elseif out_order == :ABGR
-        return ARGB32(to_N0f8(c.a), to_N0f8(c.b), to_N0f8(c.g), to_N0f8(c.r))
-    elseif out_order == :RGBA
-        return ARGB32(to_N0f8(c.r), to_N0f8(c.g), to_N0f8(c.b), to_N0f8(c.a))
-    elseif out_order == :BGRA
-        return ARGB32(to_N0f8(c.b), to_N0f8(c.g), to_N0f8(c.r), to_N0f8(c.a))
-    end
+    return ARGB32(to_N0f8(c.r), to_N0f8(c.g), to_N0f8(c.b), to_N0f8(c.a))
 end
 
 function _quantize_image(matrix, in_order::ColorOrder; colors::Int=256, limit=0, target=100)
@@ -64,41 +56,36 @@ function _quantize_image(matrix, in_order::ColorOrder; colors::Int=256, limit=0,
         throw(OutOfMemoryError())
     end
 
-    ret = @ccall libimagequant.liq_set_max_colors(attr::Ptr{Cvoid}, colors::Cint)::Cint;
+    ret = @ccall libimagequant.liq_set_max_colors(attr::Ptr{Cvoid}, colors::Cint)::Cint
     if ret != 0
         @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
         throw(ErrorException("`liq_set_max_colors` failed error code $ret"))
     end
 
-    # ret = @ccall libimagequant.liq_set_quality(attr::Ptr{Cvoid}, limit::Cint, target::Cint)::Cint;
-    # if ret != 0
-        # @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
-        # throw(ErrorException("`liq_set_quality` failed error code $ret"))
-    # end
-
-
-
-
+    input_data2 = similar(input_data)
+    copyto!(input_data2, input_data)
+    @show summary(input_data2)
     # Create image handle
     # SAFETY: input_data must be preserved until after liq_image_create_rgba returns
-    image = GC.@preserve input_data @ccall libimagequant.liq_image_create_rgba(attr::Ptr{Cvoid},
-                                                                               input_data::Ptr{Cuchar},
-                                                                               width::Cint,
-                                                                               height::Cint,
-                                                                               0::Cint)::Ptr{Cvoid}
+    image_handle = @ccall libimagequant.liq_image_create_rgba(attr::Ptr{Cvoid},
+                                                              input_data2::Ref{Cuchar},
+                                                              width::Cint,
+                                                              height::Cint,
+                                                              0::Cint)::Ptr{Cvoid}
 
-    if image == C_NULL
+    if image_handle == C_NULL
         @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
+        println("Error: liq_image_create_rgba failed")
         throw(OutOfMemoryError())
     end
 
     # Perform quantization
     res = Ref{Ptr{Cvoid}}()
-    ret = @ccall libimagequant.liq_image_quantize(image::Ptr{Cvoid},
+    ret = @ccall libimagequant.liq_image_quantize(image_handle::Ptr{Cvoid},
                                                   attr::Ptr{Cvoid},
-                                                  res::Ptr{Ptr{Cvoid}})::Cint
+                                                  res::Ref{Ptr{Cvoid}})::Cint
     if ret != 0
-        @ccall libimagequant.liq_image_destroy(image::Ptr{Cvoid})::Cvoid
+        @ccall libimagequant.liq_image_destroy(image_handle::Ptr{Cvoid})::Cvoid
         @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
         throw(ErrorException("Quantization failed with error code $ret"))
     end
@@ -112,18 +99,18 @@ function _quantize_image(matrix, in_order::ColorOrder; colors::Int=256, limit=0,
     # ret = @ccall libimagequant.liq_set_dithering_level(res[]::Ptr{Cvoid}, 1.0::Cfloat)::Cint;
     if ret != 0
         @ccall libimagequant.liq_result_destroy(res[]::Ptr{Cvoid})::Cvoid
-        @ccall libimagequant.liq_image_destroy(image::Ptr{Cvoid})::Cvoid
+        @ccall libimagequant.liq_image_destroy(image_handle::Ptr{Cvoid})::Cvoid
         @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
         throw(ErrorException("Failed to quantize image with error code $ret"))
     end
 
-    ret = GC.@preserve output_data @ccall libimagequant.liq_write_remapped_image(res[]::Ptr{Cvoid},
-                                                                                 image::Ptr{Cvoid},
-                                                                                 output_data::Ref{Cuchar},
-                                                                                 pixel_size::Cint)::Cint
+    ret = @ccall libimagequant.liq_write_remapped_image(res[]::Ptr{Cvoid},
+                                                        image_handle::Ptr{Cvoid},
+                                                        output_data::Ref{Cuchar},
+                                                        pixel_size::Cint)::Cint
     if ret != 0
         @ccall libimagequant.liq_result_destroy(res[]::Ptr{Cvoid})::Cvoid
-        @ccall libimagequant.liq_image_destroy(image::Ptr{Cvoid})::Cvoid
+        @ccall libimagequant.liq_image_destroy(image_handle::Ptr{Cvoid})::Cvoid
         @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
         throw(ErrorException("Failed to remap image with error code $ret"))
     end
@@ -131,14 +118,15 @@ function _quantize_image(matrix, in_order::ColorOrder; colors::Int=256, limit=0,
     # Get the palette
     palette_ptr = @ccall libimagequant.liq_get_palette(res[]::Ptr{Cvoid})::Ptr{LiqPalette}
     palette = unsafe_load(palette_ptr)
-
+    println(palette.count)
     # safety ? do we need this before we free the C memory?
     palette = deepcopy(palette)
-
+    output_data = deepcopy(output_data)
     # Clean up
     @ccall libimagequant.liq_result_destroy(res[]::Ptr{Cvoid})::Cvoid
-    @ccall libimagequant.liq_image_destroy(image::Ptr{Cvoid})::Cvoid
+    @ccall libimagequant.liq_image_destroy(image_handle::Ptr{Cvoid})::Cvoid
     @ccall libimagequant.liq_attr_destroy(attr::Ptr{Cvoid})::Cvoid
+    println(palette.count)
 
     return output_data, palette
 end
