cd3b49d073e30f984d45010703357dcbcfa74568
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-03-29 15:13:16 +0100

    cleanup

--- src/LibImageQuant.jl
+++ src/LibImageQuant.jl
@@ -3,118 +3,122 @@ module LibImageQuant
 using ColorTypes
 using IndirectArrays
 
-export quantize_image
+export quantize_image, LibImageQuantError
 
 # TODO- jll
 const libimagequant = "/Users/eph/libimagequant/imagequant-sys/usr/local/lib/libimagequant.dylib"
 
 include("../gen/libimagequant.jl")
-using .LibImageQuantWrapper: liq_color, liq_palette, liq_result, liq_image_create_rgba,
-                             liq_image_quantize, liq_write_remapped_image, liq_get_palette,
-                             liq_attr_create, liq_set_max_colors, liq_attr_destroy,
+
+# objects
+using .LibImageQuantWrapper: liq_color, liq_palette, liq_result, liq_error, LIQ_OK
+
+# functions
+using .LibImageQuantWrapper: liq_image_create_rgba,
+                             liq_image_quantize, liq_write_remapped_image, liq_get_palette
+
+# cleanup
+using .LibImageQuantWrapper: liq_attr_destroy,
                              liq_result_destroy, liq_image_destroy
+
+struct LibImageQuantError <: Exception
+    prefix::String
+    code::liq_error
+end
+function Base.showerror(io::IO, e::LibImageQuantError)
+    return print(io, "LibImageQuantError: ", prefix, " with code ", e.code)
+end
 
 function jl_to_c(matrix)
     matrix = permutedims(matrix)
     matrix = ColorTypes.RGBA.(matrix)
-    input_data = collect(reinterpret(reshape, UInt8, matrix))
-    return copy(input_data)
+    return collect(reinterpret(reshape, UInt8, matrix))
 end
 
 function to_argb32(c::liq_color)
+    # why this order? it works...
     return ARGB32(to_N0f8(c.r), to_N0f8(c.g), to_N0f8(c.b), to_N0f8(c.a))
 end
 
-function _quantize_image(matrix; colors::Int=256, limit=0, target=100)
+function _quantize_image(matrix; colors, limit, target)
     height, width = size(matrix)
     input_data = jl_to_c(matrix)
 
     # Create attribute handle
+    # NOTE: we need to free it from every return path including errors
     attr = liq_attr_create()
-    # SAFETY: Need to ensure this handle is freed even if subsequent calls fail
     if attr == C_NULL
-        throw(OutOfMemoryError())
+        throw(ErrorException("`liq_attr_create` failed, potentially out of memory"))
     end
 
     ret = liq_set_max_colors(attr, Cint(colors))
-    if ret != 0
+    if ret != LIQ_OK
         liq_attr_destroy(attr)
-        throw(ErrorException("`liq_set_max_colors` failed error code $ret"))
+        throw(LibImageQuantError("`liq_set_max_colors` failed", ret))
     end
 
-    input_data2 = similar(input_data)
-    copyto!(input_data2, input_data)
-    @show summary(input_data2)
     # Create image handle
-    # SAFETY: input_data must be preserved until after liq_image_create_rgba returns
-    image_handle = liq_image_create_rgba(attr, input_data2, Cint(width), Cint(height), 0.0)
+    # NOTE: we need to free it from every return path including errors
+    image_handle = liq_image_create_rgba(attr, input_data, Cint(width), Cint(height), 0.0)
 
     if image_handle == C_NULL
         liq_attr_destroy(attr)
-        println("Error: liq_image_create_rgba failed")
-        throw(OutOfMemoryError())
+        throw(ErrorException("`liq_image_create_rgba` failed, potentially out of memory"))
     end
 
-    # Perform quantization
-    # res = liq_result()
-    # res_ref = Ref{liq_result}(Ptr{liq_result}(res))
+    # Create result object
+    # NOTE: we need to free it from every return path including errors
     res = Ref{Ptr{liq_result}}(C_NULL)
+
+    # Perform quantization
     ret = liq_image_quantize(image_handle, attr, res)
-    if ret != 0
+    if ret != LIQ_OK
         liq_image_destroy(image_handle)
         liq_attr_destroy(attr)
-        throw(ErrorException("Quantization failed with error code $ret"))
+        throw(LibImageQuantError("`liq_image_quantize` failed", ret))
     end
+
+    # set dithering? other settings?
 
     # Get the remapped image data
     output_data = Matrix{Cuchar}(undef, width, height)
     pixel_size = width * height
-
-    # size_t pixels_size = width * height;
-    # unsigned char *raw_8bit_pixels = malloc(pixels_size);
-    # ret = @ccall libimagequant.liq_set_dithering_level(res[]::Ptr{Cvoid}, 1.0::Cfloat)::Cint;
-    if ret != 0
-        liq_result_destroy(res[])
-        liq_image_destroy(image_handle)
-        liq_attr_destroy(attr)
-        throw(ErrorException("Failed to quantize image with error code $ret"))
-    end
-
     ret = liq_write_remapped_image(res[],
                                    image_handle,
                                    output_data,
                                    UInt(pixel_size))
-    if ret != 0
+    if ret != LIQ_OK
         liq_result_destroy(res[])
         liq_image_destroy(image_handle)
         liq_attr_destroy(attr)
-        throw(ErrorException("Failed to remap image with error code $ret"))
+        throw(LibImageQuantError("`liq_write_remapped_image` failed", ret))
     end
 
     # Get the palette
     palette_ptr = liq_get_palette(res[])
     palette = unsafe_load(palette_ptr)
-    println(palette.count)
 
     # safety ? do we need this before we free the C memory?
-    palette = deepcopy(palette)
-    output_data = deepcopy(output_data)
+    # palette = deepcopy(palette)
+    # output_data = deepcopy(output_data)
+
     # Clean up
     liq_result_destroy(res[])
     liq_image_destroy(image_handle)
     liq_attr_destroy(attr)
-    println(palette.count)
 
     return output_data, palette
 end
 
 to_N0f8(c::UInt8) = reinterpret(ColorTypes.N0f8, c)
 
-function quantize_image(matrix)
-    output_data, palette = _quantize_image(matrix)
+function quantize_image(matrix; colors=256, limit=0, target=100)
+    output_data, palette = _quantize_image(matrix; colors, limit, target)
     output_data = permutedims(output_data)
-    colors = to_argb32.(collect(palette.entries)[1:(palette.count)])
-    return IndirectArray(output_data .+ 1, colors)
+    color_vec = to_argb32.(collect(palette.entries)[1:(palette.count)])
+    # there's gotta be a better way than +1 here, since it converts
+    # our tiny UInt8's into Int64's. But we need our indices to be one-based. Maybe offset arrays for colors so we can use a 0-based index?
+    return IndirectArray(output_data .+ 1, color_vec)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using LibImageQuant
 using Test
 
-@testset "LibImageQuant.jl" begin
-    # Write your tests here.
-end
+using CairoMakie, PNGFiles
+
+img_dir = joinpath(pkgdir(LibImageQuant), "test", "images")
+
+fig = Figure()
+ax = Axis(fig[1, 1]; title="test")
+scatter!(ax, rand(100), rand(100) .* 10; marker=:rect, color=:red, label="red rect")
+scatter!(ax, rand(100), rand(100) .* 10; marker=:x, color=:blue, label="blue x")
+scatter!(ax, rand(100), rand(100) .* 10; marker=:star5, color=:green, label="green star")
+axislegend(ax)
+
+matrix = colorbuffer(fig)
+
+# Save originals
+PNGFiles.save(joinpath(img_dir, "test-mat.png"), matrix)
+CairoMakie.save(joinpath(img_dir, "test-cm.png"), fig)
+
+output = quantize_image(matrix)
+PNGFiles.save(joinpath(img_dir, "test-iq.png"), output)
