cdc89867249a6475bdd2f31097357b8334082aee
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-04-08 00:37:49 +0200

    wip

--- src/LibImageQuant.jl
+++ src/LibImageQuant.jl
@@ -18,7 +18,11 @@ using .LibImageQuantWrapper: liq_attr_create,
                              liq_image_create_rgba,
                              liq_image_quantize, liq_write_remapped_image,
                              liq_set_max_colors,
-                             liq_get_palette
+                             liq_get_palette,
+                             liq_set_quality,
+                             liq_set_speed,
+                             liq_set_min_posterization,
+                             liq_set_dithering_level
 
 # cleanup
 using .LibImageQuantWrapper: liq_attr_destroy,
@@ -45,7 +49,7 @@ function to_argb32(c::liq_color)
     return ARGB32(to_N0f8(c.r), to_N0f8(c.g), to_N0f8(c.b), to_N0f8(c.a))
 end
 
-function _quantize_image_matrix(matrix; colors)
+function _quantize_image_matrix(matrix; colors, quality, speed, dither, posterize)
     height, width = size(matrix)
     input_data = jl_to_c(matrix)
 
@@ -56,63 +60,64 @@ function _quantize_image_matrix(matrix; colors)
         throw(ErrorException("`liq_attr_create` failed, potentially out of memory"))
     end
 
-    ret = liq_set_max_colors(attr, Cint(colors))
-    if ret != LIQ_OK
+    try
+        # Configure the attributes
+        ret = liq_set_max_colors(attr, Cint(colors))
+        ret != LIQ_OK && throw(LibImageQuantError("`liq_set_max_colors` failed", ret))
+
+        ret = liq_set_quality(attr, Cint(quality[1]), Cint(quality[2]))
+        ret != LIQ_OK && throw(LibImageQuantError("`liq_set_quality` failed", ret))
+
+        ret = liq_set_speed(attr, Cint(speed))
+        ret != LIQ_OK && throw(LibImageQuantError("`liq_set_speed` failed", ret))
+
+        if posterize > 0
+            ret = liq_set_min_posterization(attr, Cint(posterize))
+            ret != LIQ_OK &&
+                throw(LibImageQuantError("`liq_set_min_posterization` failed", ret))
+        end
+
+        # Create image handle
+        image_handle = liq_image_create_rgba(attr, input_data, Cint(width), Cint(height),
+                                             0.0)
+        image_handle == C_NULL && throw(ErrorException("`liq_image_create_rgba` failed"))
+
+        try
+            # Create result object and quantize
+            res = Ref{Ptr{liq_result}}(C_NULL)
+            ret = liq_image_quantize(image_handle, attr, res)
+            ret != LIQ_OK && throw(LibImageQuantError("`liq_image_quantize` failed", ret))
+
+            try
+                # Set dithering level (0 to 1)
+                ret = liq_set_dithering_level(res[], Float32(dither))
+                ret != LIQ_OK &&
+                    throw(LibImageQuantError("`liq_set_dithering_level` failed", ret))
+
+                # Get remapped image
+                output_data = Matrix{Cuchar}(undef, width, height)
+                pixel_size = width * height
+                ret = liq_write_remapped_image(res[],
+                                               image_handle,
+                                               output_data,
+                                               UInt(pixel_size))
+                ret != LIQ_OK &&
+                    throw(LibImageQuantError("`liq_write_remapped_image` failed", ret))
+
+                # Get palette
+                palette_ptr = liq_get_palette(res[])
+                palette = unsafe_load(palette_ptr)
+
+                return output_data, palette
+            finally
+                liq_result_destroy(res[])
+            end
+        finally
+            liq_image_destroy(image_handle)
+        end
+    finally
         liq_attr_destroy(attr)
-        throw(LibImageQuantError("`liq_set_max_colors` failed", ret))
     end
-
-    # Create image handle
-    # NOTE: we need to free it from every return path including errors
-    image_handle = liq_image_create_rgba(attr, input_data, Cint(width), Cint(height), 0.0)
-
-    if image_handle == C_NULL
-        liq_attr_destroy(attr)
-        throw(ErrorException("`liq_image_create_rgba` failed, potentially out of memory"))
-    end
-
-    # Create result object
-    # NOTE: we need to free it from every return path including errors
-    res = Ref{Ptr{liq_result}}(C_NULL)
-
-    # Perform quantization
-    ret = liq_image_quantize(image_handle, attr, res)
-    if ret != LIQ_OK
-        liq_image_destroy(image_handle)
-        liq_attr_destroy(attr)
-        throw(LibImageQuantError("`liq_image_quantize` failed", ret))
-    end
-
-    # set dithering? other settings?
-
-    # Get the remapped image data
-    output_data = Matrix{Cuchar}(undef, width, height)
-    pixel_size = width * height
-    ret = liq_write_remapped_image(res[],
-                                   image_handle,
-                                   output_data,
-                                   UInt(pixel_size))
-    if ret != LIQ_OK
-        liq_result_destroy(res[])
-        liq_image_destroy(image_handle)
-        liq_attr_destroy(attr)
-        throw(LibImageQuantError("`liq_write_remapped_image` failed", ret))
-    end
-
-    # Get the palette
-    palette_ptr = liq_get_palette(res[])
-    palette = unsafe_load(palette_ptr)
-
-    # safety ? do we need this before we free the C memory?
-    # palette = deepcopy(palette)
-    # output_data = deepcopy(output_data)
-
-    # Clean up
-    liq_result_destroy(res[])
-    liq_image_destroy(image_handle)
-    liq_attr_destroy(attr)
-
-    return output_data, palette
 end
 
 to_N0f8(c::UInt8) = reinterpret(ColorTypes.N0f8, c)
@@ -123,33 +128,55 @@ to_matrix(val) = val
 
 # the outer function just calls `to_matrix` then dispaches to `quantize_image_matrix`
 """
-    quantize_image(image; colors=256)
+    quantize_image(val; colors=256, quality=(0,100), speed=4, dither=1.0, posterize=0)
 
-Uses libimagequant to quantize an image to a limited number of colors between 2 and 256,
-with 256 as the default. This can reduce the image size when saving as a PNG.
+Uses libimagequant to quantize an image to a limited number of colors.
+Returns an `IndirectArray`, which is saved efficiently by PNGFiles.jl (used by default with CairoMakie when saving `.png` files).
 
-Returns an `IndirectArray`, which is saved efficiently by PNGFiles.jl (used by default
-with CairoMakie when saving `.png` files).
+## Arguments
+
+- `colors::Int=256`: number of colors (2-256)
+- `quality::Tuple{Int,Int}=(0,100)`: don't save below min, use fewer colors below max quality
+- `speed::Int=4`: speed/quality trade-off. 1=slow, 4=default, 10=fast
+- `dither::Number=1.0`: dithering level between 0 (none) and 1 (full)
+- `posterize::Int=0`: output lower-precision color (0=default, 1-4 for lower precision)
 
 ## Example
 
 ```julia
 using CairoMakie, LibImageQuant
-
 fig = scatter(rand(1000), rand(1000))
+save("test-original.png", fig)
 save("test-256.png", quantize_image(fig))
 save("test-8.png", quantize_image(fig; colors=8))
-save("test-original.png", fig)
 ```
+
 """
 quantize_image(val::Any; kw...) = quantize_image_matrix(to_matrix(val); kw...)
 
-function quantize_image_matrix(matrix::AbstractMatrix{T}; colors=256) where {T}
-    # we will get more confusing errors from the C library so better to throw here
+function quantize_image_matrix(matrix::AbstractMatrix{T};
+                               colors::Integer=256,
+                               quality::Tuple{Integer,Integer}=(0, 100),
+                               speed::Integer=4,
+                               dither::Number=1.0,
+                               posterize::Integer=0) where {T}
+
+    # Parameter validation
     isempty(matrix) && throw(ArgumentError("matrix is empty"))
-    # TODO- support the options pngquant supports
-    output_data, palette = _quantize_image_matrix(matrix; colors)
+    !(2 <= colors <= 256) && throw(ArgumentError("colors must be between 2 and 256"))
+    !(1 <= speed <= 10) && throw(ArgumentError("speed must be between 1 and 10"))
+    !(0 <= dither <= 1) && throw(ArgumentError("dither must be between 0 and 1"))
+    !(0 <= posterize <= 4) && throw(ArgumentError("posterize must be between 0 and 4"))
+    quality_min, quality_max = quality
+    !(0 <= quality_min <= 100 && 0 <= quality_max <= 100) &&
+        throw(ArgumentError("quality values must be between 0 and 100"))
+    quality_min > quality_max &&
+        throw(ArgumentError("minimum quality cannot be greater than maximum"))
+
+    output_data, palette = _quantize_image_matrix(matrix; colors, quality, speed, dither,
+                                                  posterize)
     output_data = permutedims(output_data)
+
     color_vec = to_argb32.(collect(palette.entries)[1:(palette.count)])
     # we need our indices to be 1-based, so we have to promote from UInt8 to Int16 to get the range 1:256.
     return IndirectArray(output_data .+ Int16(1), color_vec)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,12 @@ using IndirectArrays, ColorTypes
 using CairoMakie
 using Random
 
+# Setup test environment
 img_dir = joinpath(pkgdir(LibImageQuant), "test", "images")
 isdir(img_dir) && rm(img_dir; force=true, recursive=true)
 mkpath(img_dir)
 
+# Create test figure
 Random.seed!(1234)
 fig = Figure()
 ax = Axis(fig[1, 1]; title="test")
@@ -15,51 +17,110 @@ scatter!(ax, rand(100), rand(100) .* 10; marker=:rect, color=:red, label="red re
 scatter!(ax, rand(100), rand(100) .* 10; marker=:x, color=:blue, label="blue x")
 scatter!(ax, rand(100), rand(100) .* 10; marker=:star5, color=:green, label="green star")
 axislegend(ax)
-
-# Save originals
-save(joinpath(img_dir, "test-cm.png"), fig)
-output = quantize_image(fig)
-@test output isa IndirectArray
-@test output.index isa Matrix{Int16}
-@test output.values isa Vector{ColorTypes.ARGB32}
-path = joinpath(img_dir, "test-iq.png")
-save(path, output)
-# we could do some image testing here to automatically compare the two,
-# but for now I will inspect them manually
-
-@testset "Errors" begin
-    @test_throws LibImageQuantError quantize_image(fig; colors=0)
-    @test_throws LibImageQuantError quantize_image(fig; colors=257)
-    @test_throws MethodError quantize_image([])
-    @test_throws ArgumentError quantize_image(reshape([], 0, 0))
-    @test_throws ArgumentError quantize_image(zeros(UInt32, 0, 0))
-end
-
 matrix = colorbuffer(fig)
-# this is more for seeing how quantization affects sizes
-# than truly testing the wrapper, but we might as well call `quantize_image`
-# in a few cases here in the tests
-@testset "Quantization shrinks size in common case" begin
-    orig_size = filesize(joinpath(img_dir, "test-cm.png"))
-    for q in 1:8
-        N = 2^q
-        output = quantize_image(fig; colors=N)
+
+@testset "LibImageQuant.jl" begin
+    @testset "Basic functionality" begin
+        save(joinpath(img_dir, "test-cm.png"), fig)
+        output = quantize_image(fig)
         @test output isa IndirectArray
         @test output.index isa Matrix{Int16}
         @test output.values isa Vector{ColorTypes.ARGB32}
-        @test length(output.values) <= N
         @test size(output) == size(matrix)
-        N_str = lpad(N, 3, "0")
-        path = joinpath(img_dir, "test-iq-$N_str.png")
-        save(path, output)
-        @test filesize(path) < orig_size
+        save(joinpath(img_dir, "test-iq.png"), output)
+    end
+
+    @testset "Invalid inputs" begin
+        @test_throws ArgumentError quantize_image(fig; colors=0)
+        @test_throws ArgumentError quantize_image(fig; colors=1)
+        @test_throws ArgumentError quantize_image(fig; colors=257)
+        @test_throws ArgumentError quantize_image(fig; speed=0)
+        @test_throws ArgumentError quantize_image(fig; speed=11)
+        @test_throws ArgumentError quantize_image(fig; dither=-0.1)
+        @test_throws ArgumentError quantize_image(fig; dither=1.1)
+        @test_throws ArgumentError quantize_image(fig; posterize=-1)
+        @test_throws ArgumentError quantize_image(fig; posterize=5)
+        @test_throws ArgumentError quantize_image(fig; quality=(-1, 100))
+        @test_throws ArgumentError quantize_image(fig; quality=(0, 101))
+        @test_throws ArgumentError quantize_image(fig; quality=(50, 40))
+        @test_throws MethodError quantize_image([])
+        @test_throws ArgumentError quantize_image(reshape([], 0, 0))
+        @test_throws ArgumentError quantize_image(zeros(UInt32, 0, 0))
+    end
+
+    @testset "Parameter ranges" begin
+        @testset "Color count" begin
+            for colors in [2, 8, 16, 128, 256]
+                output = quantize_image(fig; colors)
+                @test output isa IndirectArray
+                @test length(output.values) <= colors
+            end
+        end
+
+        @testset "Speed settings" begin
+            for speed in [1, 4, 10]
+                output = quantize_image(fig; speed)
+                @test output isa IndirectArray
+            end
+        end
+
+        @testset "Dithering levels" begin
+            for dither in [0.0, 0.5, 1.0]
+                output = quantize_image(fig; dither)
+                @test output isa IndirectArray
+            end
+        end
+
+        @testset "Posterization levels" begin
+            for posterize in [0, 1, 2, 3, 4]
+                output = quantize_image(fig; posterize)
+                @test output isa IndirectArray
+            end
+        end
+
+        @testset "Quality ranges" begin
+            for quality in [(0, 100), (20, 80), (50, 50)]
+                output = quantize_image(fig; quality)
+                @test output isa IndirectArray
+            end
+        end
+    end
+
+    @testset "Combined parameters" begin
+        output = quantize_image(fig;
+                               colors=16,
+                               speed=4,
+                               dither=0.5,
+                               posterize=2,
+                               quality=(30, 70))
+        @test output isa IndirectArray
+        @test length(output.values) <= 16
+    end
+
+    @testset "File size reduction" begin
+        orig_size = filesize(joinpath(img_dir, "test-cm.png"))
+        for q in 1:8
+            N = 2^q
+            output = quantize_image(fig; colors=N)
+            @test output isa IndirectArray
+            @test output.index isa Matrix{Int16}
+            @test output.values isa Vector{ColorTypes.ARGB32}
+            @test length(output.values) <= N
+            @test size(output) == size(matrix)
+            N_str = lpad(N, 3, "0")
+            path = joinpath(img_dir, "test-iq-$N_str.png")
+            save(path, output)
+            @test filesize(path) < orig_size
+        end
+    end
+
+    @testset "Gradient image" begin
+        uint32s = reshape(UInt32.([range(typemin(UInt32), typemax(UInt32); step=0x0000431c);
+                                  zero(UInt32)]), 500, 500)
+        matrix = reinterpret(ARGB32, uint32s)
+        save(joinpath(img_dir, "test-gradient.png"), matrix)
+        output = quantize_image(matrix)
+        save(joinpath(img_dir, "test-gradient-iq.png"), output)
+        @test output isa IndirectArray
     end
 end
-
-uint32s = reshape(UInt32.([range(typemin(UInt32), typemax(UInt32); step=0x0000431c);
-                           zero(UInt32)]), 500, 500)
-matrix = reinterpret(ARGB32, uint32s)
-save(joinpath(img_dir, "test-gradient.png"), matrix)
-
-output = quantize_image(matrix)
-save(joinpath(img_dir, "test-gradient-iq.png"), output)
